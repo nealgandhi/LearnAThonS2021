36d8c56efed7c9ee2264cca54b26700fa8485c69
Alex Habich <contact@alexhabi.ch>
2021-04-04 12:38:42 -0500

    Only define admin url once

--- document/urls.py
+++ document/urls.py
@@ -6,5 +6,4 @@ from document.views import editor, delete_document
 urlpatterns = [
     path('', editor, name='editor'),
     path('delete_document/<int:docid>/', delete_document, name='delete_document'),
-    path('admin/', admin.site.urls),
 ]
