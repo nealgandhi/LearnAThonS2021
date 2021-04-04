d27f58ffc084fa0eef0b0c32e4796869bb25aabf
Megan Vu <75195965+MeganVu@users.noreply.github.com>
2021-04-04 15:28:52 -0500

    Revert "Merge branch 'main' of https://github.com/nealgandhi/LearnAThonS2021 into main"
    This reverts commit e0a1e7c880556bc0e65a0bd9ea872caa8f259880, reversing
    changes made to ec9715764b4f33df2ee8ac1d7302c0661a17a882.

--- document/views.py
+++ document/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 
 from .models import Document
 
-
 def editor(request):
     docid = int(request.GET.get('docid', 0))
     documents = Document.objects.all()
@@ -36,7 +35,6 @@ def editor(request):
     }
 
     return render(request, 'editor.html', context)
-
 
 def delete_document(request, docid):
     document = Document.objects.get(pk=docid)
--- notes/urls.py
+++ notes/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from document.views import editor, delete_document
 
 urlpatterns = [
+    path('', editor, name='editor'),
+    path('delete_document/<int:docid>/', delete_document, name='delete_document'),
     path('admin/', admin.site.urls),
-    path('doc/', include('document.urls')),
 ]
